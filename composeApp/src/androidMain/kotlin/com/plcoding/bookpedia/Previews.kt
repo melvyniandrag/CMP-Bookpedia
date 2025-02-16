590224ab78b9d02601cc18b4cbfb76652d5def77
Melvyn Drag Laptop <melvyniandrag@gmail.com>
2025-02-16 16:13:33 -0500

    Book list page working, we can see search results along with the favorites.

--- composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
@@ -8,6 +8,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.book.presentation.book_list.BookListScreen
+import com.plcoding.bookpedia.book.presentation.book_list.BookListState
 import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
 
 @Preview()
@@ -28,4 +31,31 @@ private fun BookSearchBarPreview(){
             )
         }
     }
+}
+
+private val books = (1..100).map {
+    Book(
+        id = it.toString(),
+        title = "Book ${it}",
+        imageUrl = "https://test.com",
+        authors = listOf("Melvyn Drag"),
+        description = "Description ${it}",
+        firstPublishedYear = null,
+        averageRating = 4.3325,
+        ratingCount = 5,
+        languages = emptyList(),
+        numPages = 100,
+        numEditions = 3
+    )
+}
+
+@Preview()
+@Composable
+private fun BookListScreenPreview(){
+    BookListScreen(
+        state = BookListState(
+            searchResults = books
+        ),
+        onAction = {}
+    )
 }
--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListState.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListState.kt
@@ -3,9 +3,25 @@ package com.plcoding.bookpedia.book.presentation.book_list
 import com.plcoding.bookpedia.book.domain.Book
 import com.plcoding.bookpedia.core.presentation.UiText
 
+val books = (1..100).map {
+    Book(
+        id = it.toString(),
+        title = "Book ${it}",
+        imageUrl = "https://test.com",
+        authors = listOf("Melvyn Drag"),
+        description = "Description ${it}",
+        firstPublishedYear = null,
+        averageRating = 4.3325,
+        ratingCount = 5,
+        languages = emptyList(),
+        numPages = 100,
+        numEditions = 3
+    )
+}
+
 data class BookListState(
     val searchQuery: String = "Kotlin",
-    val searchResults: List<Book> = emptyList(),
+    val searchResults: List<Book> = books,
     val favoriteBooks: List<Book> = emptyList(),
     val isLoading: Boolean = false,
     val selectedTabIndex: Int = 0,
