fdcd79d08a0cf3f22848dd4f9da7a553adac2574
Melvyn Drag Laptop <melvyniandrag@gmail.com>
2025-02-25 12:52:02 -0500

    4:24:00 descriptions are loading. Be sure to click on a book that has a description, such as a Harry Potter book.

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/network/KtorRemoteBookDataSource.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/network/KtorRemoteBookDataSource.kt
@@ -1,5 +1,6 @@
 package com.plcoding.bookpedia.book.data.network
 
+import com.plcoding.bookpedia.book.data.dto.BookWorkDto
 import com.plcoding.bookpedia.book.data.dto.SearchResponseDto
 import com.plcoding.bookpedia.book.domain.Book
 import com.plcoding.bookpedia.core.domain.Result
@@ -15,7 +16,7 @@ class KtorRemoteBookDataSource(
     private val httpClient: HttpClient
 ) : RemoteBookDataSource{
     override suspend fun searchBooks(query: String, resultLimit: Int?): Result<SearchResponseDto, DataError.Remote> {
-        return safeCall{
+        return safeCall<SearchResponseDto>{
             httpClient.get(
                 urlString = "$BASE_URL/search.json"
             ){
@@ -24,6 +25,14 @@ class KtorRemoteBookDataSource(
                 parameter("language", "eng")
                 parameter("fields", "key,title,language,cover_i,author_key,author_name,cover_edition_key,first_publish_year,ratings_average,ratings_count,number_of_pages_median,edition_count")
             }
+        }
+    }
+
+    override suspend fun getBookDetails(bookWorkId: String): Result<BookWorkDto, DataError.Remote> {
+        return safeCall<BookWorkDto> {
+            httpClient.get(
+                urlString = "$BASE_URL/works/$bookWorkId.json"
+            )
         }
     }
 }
--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/BookDetailScreen.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/BookDetailScreen.kt
@@ -171,14 +171,6 @@ private fun BookDetailScreen(
                 )
                 if(state.isLoading){
                     CircularProgressIndicator()
-//
-//                    Box(
-//                        modifier = Modifier
-//                            .fillMaxWidth()
-//                            .weight(1f),
-//                        contentAlignment = Alignment.Center
-//                    ){
-//                    }
                 }
                 else {
                     Text(
