35d98ec318b85c05c93af1603cbf0cd7d712249f
Melvyn Drag Laptop <melvyniandrag@gmail.com>
2025-02-27 20:27:35 -0500

    5:16:00 Animations done! Time to test on multiple platforms. So far only tested on droid.

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/app/App.kt
@@ -1,5 +1,7 @@
 package com.plcoding.bookpedia.app
 
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -36,7 +38,12 @@ fun App() {
             navigation<Route.BookGraph>(
                 startDestination = Route.BookList
             ){
-                composable<Route.BookList>{
+                composable<Route.BookList>(
+                    exitTransition = { slideOutHorizontally() },
+                    popEnterTransition = {
+                        slideInHorizontally()
+                    }
+                ){
                     val viewModel = koinViewModel<BookListViewModel>()
                     val selectedBookViewModel = it.sharedKoinViewModel<SelectedBookViewModel>(navController)
                     BookListScreenRoot(
@@ -47,7 +54,16 @@ fun App() {
                         }
                     )
                 }
-                composable<Route.BookDetail>{
+                composable<Route.BookDetail>(
+                    enterTransition = { slideInHorizontally{ initialOffset ->
+                                            initialOffset
+                                        }
+                                      },
+                    exitTransition = { slideOutHorizontally{ initialOffset ->
+                            initialOffset
+                        }
+                    }
+                ){
                     val selectedBookViewModel = it.sharedKoinViewModel<SelectedBookViewModel>(navController)
                     val viewModel = koinViewModel<BookDetailViewModel>()
                     val selectedBook by selectedBookViewModel.selectedBook.collectAsStateWithLifecycle()
--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/components/BlurredImageBackground.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/components/BlurredImageBackground.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -46,6 +47,7 @@ import cmp_bookpedia.composeapp.generated.resources.remove_from_favorites
 import coil3.compose.rememberAsyncImagePainter
 import com.plcoding.bookpedia.core.presentation.DarkBlue
 import com.plcoding.bookpedia.core.presentation.DesertWhite
+import com.plcoding.bookpedia.core.presentation.PulseAnimation
 import com.plcoding.bookpedia.core.presentation.SandYellow
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
@@ -138,7 +140,16 @@ fun BlurredImageBackground(
                     targetState = imageLoadResult
                 ){ result ->
                     when(result){
-                        null -> CircularProgressIndicator()
+                        null -> {
+                            Box(
+                                modifier = Modifier.fillMaxSize(),
+                                contentAlignment = Alignment.Center) {
+                                PulseAnimation(
+                                    modifier = Modifier.size(60.dp)
+                                        .align(Alignment.Center)
+                                )
+                            }
+                        }
                         else -> {
                             Box {
                                 Image(
